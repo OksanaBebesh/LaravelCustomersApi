ca934cdd06252e65dd2ef8751f32d32cb3b09eb4
Oksana Bebesh <oksana.bebesh1984@gmail.com>
2024-05-07 10:31:16 +0200

    feat: make simple route, rename Customer

--- project/routes/api.php
+++ project/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 use App\Http\Controllers\Api\CustomersController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/customers', CustomersController::class);
